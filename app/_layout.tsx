8c9af5ec8c060c1e0ec830363df22b005dd85614
chung <chungyueh@wuchongyuedeMacBook-Pro.local>
2025-02-13 02:03:45 +0800

    index不顯示上方導覽列

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -38,6 +38,8 @@ export default function RootLayout() {
         }}
         />
         <Stack.Screen name="+not-found" />
+        <Stack.Screen name="index" options={{ headerShown: false, }} />
+
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
