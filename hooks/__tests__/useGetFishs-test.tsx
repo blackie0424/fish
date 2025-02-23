4caa0dfbfabc232dca4b139c14916126857377cd
chung <chungyueh@wuchongyuedeMacBook-Pro.local>
2025-02-24 00:08:37 +0800

    模擬使用者按下按鈕後呼叫getfishsFromAPI，先假設資料有成功回傳，同時將isloading設為false，表示已經沒有在讀取資料

--- hooks/__tests__/useGetFishs-test.tsx
+++ hooks/__tests__/useGetFishs-test.tsx
@@ -1,12 +1,13 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen,act } from '@testing-library/react';
 import useGetFishs from '../useGetFishs';
 
 const TestComponent = () => {
-    const { fishs, isLoading } = useGetFishs();
+    const { fishs, isLoading, getFishsFromAPI } = useGetFishs();
     return (
         <div>
             <span data-testid="loading">{isLoading.toString()}</span>
             <span data-testid="fishs">{fishs.length}</span>
+            <button onClick={getFishsFromAPI}>Fetch</button>
         </div>
     );
 };
@@ -17,4 +18,20 @@ describe('useGetFishs', () => {
         expect(screen.getByTestId('loading').textContent).toBe('true');
         expect(screen.getByTestId('fishs').textContent).toBe('0');
     });
+
+    test('should update state when fetching succeeds', async () => {
+        global.fetch = jest.fn().mockResolvedValue({
+            ok: true,
+            json: async () => ({ message: "success", data: [{ id: 1, name: 'cilat' }] }),
+        });
+
+        render(<TestComponent />);
+        await act(async () => {
+            await screen.getByText('Fetch').click(); // 模擬使用者點擊
+        });
+
+        expect(screen.getByTestId('loading').textContent).toBe('false');
+        expect(screen.getByTestId('fishs').textContent).toBe('1');
+    });
+
 });
